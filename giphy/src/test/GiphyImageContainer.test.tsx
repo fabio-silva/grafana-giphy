db7c450f80adb68a047a4be9cbc8339da10a475e
Fabio Silva <ffjs1993@gmail.com>
2020-10-09 13:03:32 +0100

    test(GiphyImageContainer): add snapshot

--- giphy/src/test/GiphyImageContainer.test.tsx
+++ giphy/src/test/GiphyImageContainer.test.tsx
@@ -7,4 +7,22 @@ describe('GiphyImageContainer', () => {
     const container = shallow(<GiphyImageContainer images={[]}/>);
     expect(container.html().includes('No result found')).toBe(true);
   });
+
+  it('should display an empty node when null is passed', () => {
+    const container = shallow(<GiphyImageContainer images={null}/>);
+    expect(container.html().length).toBe(0);
+  });
+
+  it('should render one GiphyImage per image passed', () => {
+    const images = [{
+      id: '1',
+      url: 'url_1'
+    }, {
+      id: '2',
+      url: 'url_2'
+    }];
+    const container = shallow(<GiphyImageContainer images={images}/>);
+    expect(container.children().length).toBe(2);
+    expect(container).toMatchSnapshot();
+  });
 });
