6800fa33208459ae7287678cea64701762655c64
Fabio Silva <ffjs1993@gmail.com>
2020-10-09 13:10:01 +0100

    refactor: fix indentation

--- giphy/src/GiphyImage.tsx
+++ giphy/src/GiphyImage.tsx
@@ -3,16 +3,16 @@ import { css } from 'emotion';
 import { ImageProps } from './types';
 
 export const GiphyImage: React.FC<ImageProps> = ({ url = '' }) => (
-	/**
-	 * Even though the Giphy API will return same-height images, using object-fit
-	 * can avoid different heights in the future.
-	 */
-	<img
-		className={css`
-			object-fit: contain;
-			height: 100%;
-		`}
-		src={`${url}`}
-	>
-	</img>
+  /**
+   * Even though the Giphy API will return same-height images, using object-fit
+   * can avoid different heights in the future.
+   */
+  <img
+      className={css`
+          object-fit: contain;
+          height: 100%;
+      `}
+      src={`${url}`}
+  >
+  </img>
 );
--- giphy/src/GiphyImageContainer.tsx
+++ giphy/src/GiphyImageContainer.tsx
@@ -5,35 +5,35 @@ import { GiphyImage } from 'GiphyImage';
 import { ImageData } from 'types';
 
 const styles = stylesFactory(() => ({
-	giphyImg: css`
-      height: 150px;
-      margin-bottom: 10px;
-      &:not(:last-child) {
-        margin-right: 10px;
-      }
+  giphyImg: css`
+    height: 150px;
+    margin-bottom: 10px;
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
 	`
 }))();
 
 interface ImageProps {
-	images: ImageData[] | null
+  images: ImageData[] | null
 };
 
 export const GiphyImageContainer: React.FC<ImageProps> = ({ images }) => {
-	let imageEl: JSX.Element[] | JSX.Element = <></>;
-	// At this point, we can check if we should show a "no results found"
-	if (images) {
-		if (images.length) {
-			imageEl = images.map(({ id, url }) => <div
-				className={cx(styles.giphyImg)}
-				key={id}
-			>
-				<GiphyImage url={url} />
-			</div>
-			)
-		} else {
-			imageEl = <p>No result found</p>;
-		}
-	}
+  let imageEl: JSX.Element[] | JSX.Element = <></>;
+  // At this point, we can check if we should show a "no results found"
+  if (images) {
+    if (images.length) {
+      imageEl = images.map(({ id, url }) => <div
+        className={cx(styles.giphyImg)}
+        key={id}
+      >
+        <GiphyImage url={url} />
+      </div>
+      )
+    } else {
+      imageEl = <p>No result found</p>;
+    }
+  }
 
-	return <>{imageEl}</>;
+  return <>{imageEl}</>;
 }
